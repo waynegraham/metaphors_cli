51c6a76404f4b562ed488820c1beb93f96679273
Wayne Graham <wayne.graham@virginia.edu>
2013-06-05 14:56:37 -0400

    Updates the metaphors script to generate multiple years of stuff

--- metaphors.rb
+++ metaphors.rb
@@ -5,33 +5,56 @@ require 'rsolr'
 require 'rsolr-ext'
 
 TARGET = 'http://sds3.itc.virginia.edu:8080/metaphors_solr/'
-TABLE_BORDER = "+" + "-" * 78 + "+"
+TABLE_BORDER = "+" + "-" * 27 + "+"
 
 # Query syntax http://lucene.apache.org/java/2_4_0/queryparsersyntax.html
 
+$solr = RSolr::Ext.connect :url => TARGET
 
-solr = RSolr::Ext.connect :url => TARGET
+def generate_report(term)
 
-solr_params = {
-  :queries => {:categories => 'Fetters', :work_year => '171*'},
-  :facets => {:fields => ['work_genres']}
-}
+  (1600..1800).step(10).each do |year|
+    response = query_solr(term, year)
 
-response = solr.find solr_params
-
-puts response.params
-
-puts TABLE_BORDER
-
-puts "|" + sprintf("%20s", "") + "|" + sprintf("%6s", "1710") + "|"
-
-puts TABLE_BORDER
-response.facets.each do |facet|
-  facet.items.each do |item|
-    puts "|" + sprintf("%20s", item.value) + "|" + sprintf("%6d", item.hits) + "|" if item.hits > 0
+    puts TABLE_BORDER
+    puts print_header(term,year)
+    puts TABLE_BORDER
+    puts print_rows(response)
+    puts TABLE_BORDER
   end
-end if response.ok?
+end
 
-puts TABLE_BORDER
+def print_rows(response)
+  result = ""
+  response.facets.each do |facet|
+    facet.items.each do |item|
+      result += "|" + sprintf("%20s", item.value) + "|" + sprintf("%6d", item.hits) + "|\n" if item.hits > 0
+    end
+  end if response.ok?
+  result
+end
+
+def print_header(term, year)
+  "|" + sprintf("%20s", term) + "|" + sprintf("%6s", "#{year}") + "|"
+end
+
+def query_solr(term, year)
+
+  work_year = year.to_s.chop! + "*"
+  solr_params = {
+    :queries => { :categories => term, :work_year => work_year },
+    :facets => { :fields => ['work_genres'] }
+  }
+
+  $solr.find solr_params
+
+end
+
+terms = %w(Fetters Architecture)
+
+terms.each do |term|
+  puts generate_report(term)
+end
+
 
 
